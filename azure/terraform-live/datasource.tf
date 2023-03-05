b4d6bbe9b92db87be2e8b9e2d619e397e1dfaa65
leomozzer <mozzer.ferraz@gmail.com>
2023-03-05 15:58:44 -0300

    resources deployment - local test

--- azure/terraform-live/datasource.tf
+++ azure/terraform-live/datasource.tf
@@ -1,5 +1,1 @@
-data "azurerm_resource_group" "resource_group" {
-  name = "${var.app_name}-${var.environment}-rg"
-}
-
 data "azurerm_client_config" "current" {}
--- azure/terraform-live/main.tf
+++ azure/terraform-live/main.tf
@@ -9,10 +9,15 @@ resource "random_password" "db_root_pwd" {
   override_special = "!#$%&*-_=+:?"
 }
 
-resource "azurerm_key_vault" "example" {
+resource "azurerm_resource_group" "resource_group" {
+  name     = "${var.app_name}-${var.environment}-rg"
+  location = var.location
+}
+
+resource "azurerm_key_vault" "keyvault" {
   name                        = "${random_string.random.result}-kv"
-  location                    = data.azurerm_resource_group.resource_group.location
-  resource_group_name         = data.azurerm_resource_group.resource_group.name
+  location                    = azurerm_resource_group.resource_group.location
+  resource_group_name         = azurerm_resource_group.resource_group.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
@@ -23,17 +28,8 @@ resource "azurerm_key_vault" "example" {
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get",
-    ]
-
     secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
+      "Get", "Set", "Delete", "List"
     ]
   }
 }
@@ -42,20 +38,31 @@ resource "azurerm_key_vault" "example" {
 module "mysql_database" {
   source = "../terraform-modules/mysql-server"
   mysql_name = random_string.random.result
-  resource_group_name = data.azurerm_resource_group.resource_group.name
-  location = data.azurerm_resource_group.resource_group.location
-  administrator_login = "root"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = azurerm_resource_group.resource_group.location
+  administrator_login = "${random_string.random.result}root"
   administrator_login_password = random_password.db_root_pwd.result
   sku_name = "B_Gen5_2"
   storage_mb = 5120
   mysql_version = "5.7"
 }
 
+resource "azurerm_key_vault_secret" "mysql_user" {
+  name         = "mysqlUser"
+  value        = "${random_string.random.result}root"
+  key_vault_id = azurerm_key_vault.keyvault.id
+}
 
-# module "app_service" {
-#   source = "../terraform-modules/linux-app-service"
-#   app_name = ""
-#   resource_group_name = module.skaylink-resourcegroup.resource_group.name
-#   location = var.location
-#   sku_name = var.sku_name
-# }
+resource "azurerm_key_vault_secret" "mysql_pwd" {
+  name         = "mysqlPassword"
+  value        = random_password.db_root_pwd.result
+  key_vault_id = azurerm_key_vault.keyvault.id
+}
+
+module "app_service" {
+  source = "../terraform-modules/linux-app-service"
+  app_name = random_string.random.result
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = azurerm_resource_group.resource_group.location
+  sku_name = var.sku_name
+}
