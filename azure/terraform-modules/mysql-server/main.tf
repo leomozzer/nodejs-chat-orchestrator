0bf66cb79a15dccb666afa9250fb19203a705e1d
leomozzer <mozzer.ferraz@gmail.com>
2023-03-26 16:49:47 -0300

    fixed terraform fmt

--- azure/terraform-modules/mysql-server/main.tf
+++ azure/terraform-modules/mysql-server/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_mysql_server" "mysql_server" {
-  name                = "${var.mysql_name}"
+  name                = var.mysql_name
   location            = var.location
   resource_group_name = var.resource_group_name
 
