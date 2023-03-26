4b6adeb92e36edb9f64471f6f7b0f879453d4d71
leomozzer <mozzer.ferraz@gmail.com>
2023-03-26 18:19:30 -0300

    Removed gatewaymodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { GatewayModule } from 'src/gateway/gateway.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Chat } from './chat/chat.entity';
@@ -14,7 +13,6 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    GatewayModule,
     ChatModule,
     RoomsModule,
     UsersModule,
