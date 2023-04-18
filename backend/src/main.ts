6e8ea764a9c7855798e629b056598c0f3999eb8b
leomozzer <mozzer.ferraz@gmail.com>
2023-04-17 21:58:20 -0300

    Removed frontend app and back to backend with join

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express'
-import { join } from 'path'
-import { IoAdapter } from '@nestjs/platform-socket.io';
-
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors({
-    origin: "http://localhost:3000"
-  })
+  app.useStaticAssets(join(__dirname, '..', 'static'))
   await app.listen( process.env.PORT || 3500 );
 }
 bootstrap();
